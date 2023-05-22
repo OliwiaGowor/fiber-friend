da3f328e8fb2f6d0275a98640c05947e4e84d706
Oliwia Gowor <oliwiagowor@gmail.com>
2023-05-22 23:25:28 +0200

    formatting in text editor works now with hotkeys

--- src/components/TextEditor/TextEditor.tsx
+++ src/components/TextEditor/TextEditor.tsx
@@ -1,18 +1,11 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 // Import the Slate editor factory.
 import { Transforms, createEditor, Editor } from 'slate';
 // Import the Slate components and React plugin.
 import { Slate, Editable, withReact } from 'slate-react';
 import BoldMark from './BoldMark';
 import isHotkey from "is-hotkey";
-
-const initialValue = [
-  {
-    object: 'block',
-    type: 'paragraph',
-    children: [{ text: 'A line of text in a paragraph.' }],
-  },
-];
+import { withHistory } from 'slate-history';
 
 const HOTKEYS = {
   "mod+b": "bold",
@@ -22,54 +15,76 @@ const HOTKEYS = {
 };
 
 export default function TextEditor() {
-  const [editor] = useState(() => withReact(createEditor()))
+  const editor = useMemo(() => withHistory(withReact(createEditor())), []);
   const [value, setValue] = useState(initialValue);
+  const renderElement = React.useCallback((props: any) => <Element {...props} />, []);
+  const renderLeaf = React.useCallback((props: any) => <Leaf {...props} />, []);
 
-  const renderElement = React.useCallback((props: any) => {
-    switch (props.element.type) {
-      case 'code':
-        return <p></p>
+  const getHotkey = (key: string): string => {
+    switch (key) {
+      case "mod+b":
+        return "bold";
+      case "mod+i":
+        return "italic";
+      case "mod+u":
+        return "underline";
+      case "mod+`":
+        return "code";
       default:
-        return <DefaultElement {...props} />
+        return '';
     }
-  }, [])
-
-  // Define a leaf rendering function that is memoized with `useCallback`.
-  const renderLeaf = React.useCallback((props: any) => {
-    return <Leaf {...props} />
-  }, [])
-
+  }
 
   return (
     <Slate editor={editor} value={value} onChange={(value: any) => setValue(value)}>
       <Editable
-      renderElement={renderElement}
+        renderElement={renderElement}
+        renderLeaf={renderLeaf}
         onKeyDown={event => {
           for (const hotkey in HOTKEYS) {
             if (isHotkey(hotkey, event)) {
               event.preventDefault();
-              const mark = hotkey;
+              const mark = getHotkey(hotkey);
               toggleMark(editor, mark);
             }
           }
         }}
-        />
+      />
     </Slate>
   );
 }
 
-const isMarkActive = (editor: any, format: string | number) => {
+const toggleMark = (editor: any, format: string) => {
   const marks = Editor.marks(editor);
-  return marks ? marks /*marks[index] <- throws error*/ === true : false;
+  for (const key in marks) {
+    if (key && key == format) {
+      Editor.removeMark(editor, format);
+      return;
+    } else {
+      Editor.addMark(editor, format, true);
+      return;
+    }
+  }
+  Editor.addMark(editor, format, true);
+
 };
 
-const toggleMark = (editor: any, format: string) => {
-  const isActive = isMarkActive(editor, format);
-
-  if (isActive) {
-    Editor.removeMark(editor, format);
-  } else {
-    Editor.addMark(editor, format, true);
+const Element = ({ attributes, children, element }: any) => {
+  switch (element.type) {
+    case "block-quote":
+      return <blockquote {...attributes}>{children}</blockquote>;
+    case "bulleted-list":
+      return <ul {...attributes}>{children}</ul>;
+    case "heading-one":
+      return <h1 {...attributes}>{children}</h1>;
+    case "heading-two":
+      return <h2 {...attributes}>{children}</h2>;
+    case "list-item":
+      return <li {...attributes}>{children}</li>;
+    case "numbered-list":
+      return <ol {...attributes}>{children}</ol>;
+    default:
+      return <p {...attributes}>{children}</p>;
   }
 };
 
@@ -93,6 +108,39 @@ const Leaf = ({ attributes, children, leaf }: any) => {
   return <span {...attributes}>{children}</span>;
 };
 
-const DefaultElement = (props: any) => {
-  return <p {...props.attributes}>{props.children}</p>
-}
+const initialValue = [
+  {
+    type: "paragraph",
+    children: [
+      { text: "This is editable " },
+      { text: "rich", bold: true },
+      { text: " text, " },
+      { text: "much", italic: true },
+      { text: " better than a " },
+      { text: "<textarea>", code: true },
+      { text: "!" }
+    ]
+  },
+  {
+    type: "paragraph",
+    children: [
+      {
+        text:
+          "Since it's rich text, you can do things like turn a selection of text "
+      },
+      { text: "bold", bold: true },
+      {
+        text:
+          ", or add a semantically rendered block quote in the middle of the page, like this:"
+      }
+    ]
+  },
+  {
+    type: "block-quote",
+    children: [{ text: "A wise quote." }]
+  },
+  {
+    type: "paragraph",
+    children: [{ text: "Try it out for yourself!" }]
+  }
+];
